f795fd2008790fce875c6725e41d4bbd49c8c6dc
NameerAlmqhem <Nms3323@gmail.com>
2025-11-09 13:28:09 +0300

    Update signup redirection and remove HTML form

--- signup.php
+++ signup.php
@@ -30,15 +30,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $result = $check->get_result();
 
   if ($result->num_rows > 0) {
-    header("Location: signup.php?error=emailExists");
+    header("Location: signup.html?error=emailExists");
     exit();
   } else {
     // إدخال المستخدم
     $insert = $conn->prepare("
-      INSERT INTO user (firstName, lastName, emailAddress, password, userType, photoFileName, topics)
-      VALUES (?, ?, ?, ?, ?, ?, ?)
+      INSERT INTO user (firstName, lastName, emailAddress, password, userType, photoFileName)
+      VALUES (?, ?, ?, ?, ?, ?)
     ");
-    $insert->bind_param("sssssss", $fname, $lname, $email, $password, $userType, $uniqueName, $topics);
+    $insert->bind_param("ssssss", $fname, $lname, $email, $password, $userType, $uniqueName);
     $insert->execute();
 
     // حفظ بيانات المستخدم في السيشن
@@ -81,87 +81,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <title>Sign Up</title>
-  <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-  <h1>Welcome to <span style="color:rgb(167, 203, 251);">EduLearn</span></h1>
-  <div class="logo">
-    <img src="images/logo.png" alt="Logo">
-  </div>
-  <h2>Sign Up</h2>
-  <a href="home.html">Back to home</a>
-
-  <?php
-  if (isset($_GET['error']) && $_GET['error'] == 'emailExists'){
-      echo "<p style='color:red; text-align:center;'>This email already exists.</p>";
-  }
-  ?>
-
-  <div class="radios">
-    <label><input type="radio" name="uType" value="learner"> Learner</label>
-    <label><input type="radio" name="uType" value="educator"> Educator</label>
-  </div>
-
-  <!-- Learner form -->
-  <form id="learnerForm" class="form-box" method="POST" action="signup.php" enctype="multipart/form-data">
-    <input type="hidden" name="userType" value="learner">
-    <h3>Learner Registration</h3>
-    <div class="info">
-      <input class="input-field" type="text" name="firstName" placeholder="First Name" required>
-      <input class="input-field" type="text" name="lastName" placeholder="Last Name" required>
-    </div>
-    <div class="info">
-      <input class="input-field" type="email" name="emailAddress" placeholder="Email" required>
-      <input class="input-field" type="password" name="password" placeholder="Password" required>
-    </div>
-    <img id="l-avatar" class="avatar" src="images/default.png" alt="Profile" />
-    <input type="file" name="photo" accept="image/*" />
-    <div>
-      <button class="btn-success" type="submit">Complete Registration</button>
-    </div>
-  </form>
-
-  <!-- Educator form -->
-  <form id="educatorForm" class="form-box" method="POST" action="signup.php" enctype="multipart/form-data" style="display:none;">
-    <input type="hidden" name="userType" value="educator">
-    <h3>Educator Registration</h3>
-    <div class="info">
-      <input class="input-field" type="text" name="firstName" placeholder="First Name" required>
-      <input class="input-field" type="text" name="lastName" placeholder="Last Name" required>
-    </div>
-    <div class="info">
-      <input class="input-field" type="email" name="emailAddress" placeholder="Email" required>
-      <input class="input-field" type="password" name="password" placeholder="Password" required>
-    </div>
-
-    <img id="e-avatar" class="avatar" src="images/default.png" alt="Profile" />
-    <input type="file" name="photo" accept="image/*" />
-
-    <div class="topics">
-      <p class="topics-title">Select your topics (choose one or more):</p>
-      <label><input type="checkbox" name="topic[]" value="HTML & CSS"> HTML & CSS</label>
-      <label><input type="checkbox" name="topic[]" value="JavaScript"> JavaScript</label>
-      <label><input type="checkbox" name="topic[]" value="Databases"> Databases</label>
-    </div>
-
-    <div>
-      <button class="btn-success" type="submit">Complete Registration</button>
-    </div>
-  </form>
-
-  <footer class="site-footer">
-    <div class="footer-brand">
-      <img src="images/logo.png" alt="Edulearn Logo" class="footer-logo">
-      <h2 class="footer-name"><span style="color:rgb(167, 203, 251);">EduLearn</span></h2>
-    </div>
-    <p class="footer-copy">&copy; 2025 Edulearn. All rights reserved.</p>
-  </footer>
-
-  <script src="script.js"></script>
-</body>
-</html>
